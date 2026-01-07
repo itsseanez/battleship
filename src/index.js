a69814f4a5462a9ee14bd979d7f9815a9a64287a
itsseanez <74938477+itsseanez@users.noreply.github.com>
2026-01-07 16:45:19 -0500

    Create player1 and player2 boards then display

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Player from './Player.js';
 const battleship = (() => {
+  //Select Opponent
   const playerTypeToggle = document.querySelector('#cb2-7');
   let playerType = 'computer';
   let playerTwoName = 'Computer';
@@ -27,7 +28,58 @@ const battleship = (() => {
     if (playerTypeToggle.checked)
       playerTwoName = document.querySelector('#player-2-name').value.trim();
     const player2 = new Player(playerTwoName, playerType);
-    console.log(player1, player2);
+
+    const main = document.querySelector('main');
+    main.innerHTML = '';
+
+    const gameDiv = document.createElement('div');
+    gameDiv.id = 'game';
+    main.append(gameDiv);
+
+    const player1Div = document.createElement('div');
+    const player1Board = document.createElement('div');
+    player1Board.classList.add('player-board');
+
+    player1.board.board.forEach((row) => {
+      let rowDiv = document.createElement('div');
+      rowDiv.classList.add('row');
+      player1Board.appendChild(rowDiv);
+
+      row.forEach(() => {
+        let element = document.createElement('div');
+        element.classList.add('column');
+        rowDiv.appendChild(element);
+      });
+    });
+    const player1Identifier = document.createElement('p');
+    player1Identifier.textContent = `${player1.name}'s board`;
+
+    player1Div.append(player1Board);
+    player1Div.append(player1Identifier);
+    player1Div.classList.add('player-div');
+    gameDiv.append(player1Div);
+
+    const player2Div = document.createElement('div');
+    const player2Board = document.createElement('div');
+    player2Board.classList.add('player-board');
+    player2.board.board.forEach((row) => {
+      let rowDiv = document.createElement('div');
+      rowDiv.classList.add('row');
+      player2Board.appendChild(rowDiv);
+
+      row.forEach(() => {
+        let element = document.createElement('div');
+        element.classList.add('column');
+        rowDiv.appendChild(element);
+      });
+    });
+    const player2Identifier = document.createElement('p');
+    player2Identifier.textContent = `${player2.name}'s board`;
+
+    player2Div.append(player2Board);
+    player2Div.append(player2Identifier);
+    player2Div.classList.add('player-div');
+    gameDiv.append(player2Div);
   };
 
   const playButton = document.querySelector('#play');
