cf2ad792aeb431c2d124931a2bf84a882d33c894
itsseanez <74938477+itsseanez@users.noreply.github.com>
2026-01-07 13:55:25 -0500

    Determine if opponent is real or computer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import Player from './Player.js';
 const battleship = (() => {
-  return console.log('wow');
+  const playerTypeToggle = document.querySelector('#cb2-7');
+  let playerType = 'computer';
+  playerTypeToggle.addEventListener('change', () => {
+    const playerDiv = document.querySelector('#player-2');
+    if (playerTypeToggle.checked) {
+      playerType = 'real';
+      let playerLabel = document.createElement('p');
+      playerLabel.textContent = 'Player 2:';
+      let playerInput = document.createElement('input');
+      playerInput.type = 'text';
+      playerInput.classList.add('player-text');
+      playerInput.id = 'player-2-name';
+      playerDiv.append(playerLabel, playerInput);
+    } else {
+      playerType = 'computer';
+      playerDiv.innerHTML = '';
+    }
+  });
+  const startGame = () => {};
 })();
